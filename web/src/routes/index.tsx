b3a45befcdeeef68de7eb8e45917979dd37c55d0
YOUNGSEOK JOO <zzoo.origin@gmail.com>
2026-03-04 18:37:26 +0900

    update

---
+++ web/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Home,
+})
+
+function Home() {
+  return (
+    <div>
+      <h1>Phys Play</h1>
+    </div>
+  )
+}
